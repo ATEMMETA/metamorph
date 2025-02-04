335e18e3849ea48e4428aa9c151eddedcfe4876d
ATEMMETA <atem.86796@gmail.com>
2025-02-04 14:01:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ import requests
 
 if st.button("Get Data"):
     try:
-        response = requests.get("http://127.0.0.1:8000/data")  # Use 127.0.0.1 here!
+        response = requests.get("http://192.168.3.201:8000/data")  # Use 127.0.0.1 here!
         response.raise_for_status()
         data = response.json()
         st.write(data)
