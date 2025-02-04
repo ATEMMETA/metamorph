b4076b7d1021e5cb7b1601a7fac4ef91a1c06a79
ATEMMETA <atem.86796@gmail.com>
2025-02-04 09:13:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new appssss")
+st.title("🎈 My new apdjdbdndbpssss")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
